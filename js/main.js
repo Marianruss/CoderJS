e19e28a7f093d4af48d035db95a005e402009efc
Marianruss <marianruss12@gmail.com>
2023-01-10 20:48:39 -0300

    created file to store recipes, changed set recipe function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var test = ["1. En primer lugar, para hacer esta tostada caprese (o caprese panini) procuraremos que la rebanada de pan no sea muy fina y que tenga una densa miga que permita y aguante la colocación del resto de los ingredientes, como se explica a continuación.\n\n2. Con la elección del pan hecha, ponemos a tostar la rebanada y mientras vamos lavando y troceando el tomate en daditos pequeños. Hecho esto, colocamos el tomate sobre la rebanada de pan, y encima ponemos la mozzarella cortada en finas lonchas.\n\n3. Una vez montada, metemos nuestra tostada caprese en el horno o en el microondas y seleccionamos la función grill (si haces las tostadas caprese en microondas, asegúrate primero de que tiene función grill) hasta que el pan esté doradito y el queso quede ligeramente fundido.\n\n4. Por último, sacamos las tostadas caprese del horno o microondas con cuidado de no quemarnos, y decoramos con las hojas de albahaca frescas. ¡Una tostada fresca y sabrosa para las cenas de verano!"];
+import { objetos } from "./recetas.js";
+
 
 function selectReceta() {
-    let end = false;
     while (end != true) {
         let ingrediente = prompt('Selecciona un ingrediente principal:\n- Queso\n- Papa');
         if (ingrediente.toLowerCase() === "queso") {
@@ -17,32 +17,19 @@ function selectReceta() {
 }
 
 
+function setRecipe(recipeNumber){
 
-function createli() {
-
-    const container = document.getElementById("recipe-list");
-
-    var ul = document.createElement('ul');
-    ul.setAttribute('id', 'test')
-
-    for (i = 0; i <= test.length - 1; i++)
-        var li = document.createElement('li')
-        li.innerHTML = test[i];
-        li.setAttribute('style', 'display: block');
-        
-        ul.appendChild(li);
-
-}
-
-function setRecipe(){
-    var instructions = test[0];
-    document.getElementById("instructions").innerText = instructions
+    let instructions = objetos[recipeNumber].instrucciones;
+    let ingredients = objetos[recipeNumber].ingredientes
+    let titulo = objetos[recipeNumber].nombreReceta;
+    document.getElementById("instructions").innerText = instructions;
+    document.getElementById("ingredients-list").innerText = ingredients;
+    document.getElementById("recipe-name").innerText = titulo;
 
 }
 
 
-setRecipe()
-createli()
+setRecipe(0)
 const receta = selectReceta();
 
 
@@ -52,7 +39,3 @@ if (receta.toLowerCase() === null) {
 } else {
     alert(`Con tus ingredientes podes cocinar ${receta}`)
 }
-
-
-
-
---
+++ js/recetas.js
@@ -0,0 +1,18 @@
+export const objetos =[{
+    nombreReceta: "Tostadas caprese",
+    ingredientes: "• 100gr de tomate\n• Tres hojas de albahaca\n• Aceite de oliva a gusto\n• Oregano a gusto\n• Dos rebanadas de pan integral o blanco\n• Ajo a gusto\n",
+    instrucciones: "1. En primer lugar, para hacer esta tostada caprese (o caprese panini) procuraremos que la rebanada de pan no sea muy fina y que tenga una densa miga que permita y aguante la colocación del resto de los ingredientes, como se explica a continuación.\n\n2. Con la elección del pan hecha, ponemos a tostar la rebanada y mientras vamos lavando y troceando el tomate en daditos pequeños. Hecho esto, colocamos el tomate sobre la rebanada de pan, y encima ponemos la mozzarella cortada en finas lonchas.\n\n3. Una vez montada, metemos nuestra tostada caprese en el horno o en el microondas y seleccionamos la función grill (si haces las tostadas caprese en microondas, asegúrate primero de que tiene función grill) hasta que el pan esté doradito y el queso quede ligeramente fundido.\n\n4. Por último, sacamos las tostadas caprese del horno o microondas con cuidado de no quemarnos, y decoramos con las hojas de albahaca frescas. ¡Una tostada fresca y sabrosa para las cenas de verano!"
+    },
+    {
+    nombreReceta: "test",
+    ingredientes: "test\ntest",
+    instrucciones: "test\ntest\ntest"
+    },
+    {
+    nombreReceta: "test0",
+    ingredientes: "test1",
+    instrucciones: "test2"
+    }
+
+
+]
