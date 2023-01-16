a72fc982e9bc0adf8b6371453b2f24818ceb8d98
Marianruss <75316577+Marianruss@users.noreply.github.com>
2023-01-16 15:45:45 -0300

    Update recetas.js

--- js/recetas.js
+++ js/recetas.js
@@ -3,21 +3,21 @@ export const recetas =[{
     ingredientes: "• 100gr de tomate\n• Tres hojas de albahaca\n• Aceite de oliva a gusto\n• Oregano a gusto\n• Dos rebanadas de pan integral o blanco\n• Ajo a gusto\n",
     instrucciones: "1. En primer lugar, para hacer esta tostada caprese (o caprese panini) procuraremos que la rebanada de pan no sea muy fina y que tenga una densa miga que permita y aguante la colocación del resto de los ingredientes, como se explica a continuación.\n\n2. Con la elección del pan hecha, ponemos a tostar la rebanada y mientras vamos lavando y troceando el tomate en daditos pequeños. Hecho esto, colocamos el tomate sobre la rebanada de pan, y encima ponemos la mozzarella cortada en finas lonchas.\n\n3. Una vez montada, metemos nuestra tostada caprese en el horno o en el microondas y seleccionamos la función grill (si haces las tostadas caprese en microondas, asegúrate primero de que tiene función grill) hasta que el pan esté doradito y el queso quede ligeramente fundido.\n\n4. Por último, sacamos las tostadas caprese del horno o microondas con cuidado de no quemarnos, y decoramos con las hojas de albahaca frescas. ¡Una tostada fresca y sabrosa para las cenas de verano!",
     veggie: true,
-    foto: "/images/recetas/tostada-caprese.jpg"
+    foto: "../images/recetas/tostada-caprese.jpg"
     },
     {
     nombreReceta: "Sandwich de pollo",
     ingredientes: "• 20 g de queso roquefort\n• 1 filete de pechuga de pollo\n• Rodajas de tomate fresco y rojo\n• 1 cucharada de mayonesa\n• Lechuga picada\n",
     instrucciones: "Trituramos un poco de queso roquefort blando (a temperatura ambiente) con el tenedor y lo ligamos todo con una cucharada de mayonesa. Montamos el sandwich untando el pan con la salsa, y cubriendo con el pollo, las rodajas de tomate y la lechuga muy bien escurrida. Servimos al momento para que el pan no se humedezca.",
     veggie: false,
-    foto: "/images/recetas/sandwich-pollo.jpg"
+    foto: "../images/recetas/sandwich-pollo.jpg"
     },
     {
     nombreReceta: "Ensalada de brocoli",
     ingredientes: "• 1 unidad de Brócoli\n• 150 gramos de Tomate cherry\n• 1 diente de Ajo\n• 1 pizca de Sal\n• 1 chorro de Aceite de oliva\n• 1 chorro de Vinagre\n",
     instrucciones: "1.Cocemos el brócoli en abundante agua hirviendo con una pizca de sal. Dejamos cocer durante 10-12 minutos.\n\n2.En una sartén sofreímos el diente de ajo y los tomates cortados por la mitad. Agregamos un poco de sal.\n\n3.Cortamos el brócoli en arbolitos pequeños y agregamos los tomates cherry salteados. El ajo lo retiramos.\n\n4.Para servir la ensalada de brócoli y tomates, podemos agregar un poco más de sal si lo requiere, pimienta negra molida y un chorrito de vinagre de vino blanco.",
     veggie: true,
-    foto: "/images/recetas/ensalada-brocoli.jpg"
+    foto: "../images/recetas/ensalada-brocoli.jpg"
     }
 
 
